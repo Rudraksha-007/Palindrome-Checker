f5bf07b040bbc58ee66aac6732f8e9e5f277d207
Rudraksha-007 <rudrakshadwivedi18@gmail.com>
2024-07-28 00:42:44 +0530

    refactoring done

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const input=document.getElementById("strinp");
-const button=document.getElementById("check-btn");
-const res=document.getElementById("result");
-function check(){
-    if(input.value.length==0){
+const input = document.getElementById("strinp");
+const button = document.getElementById("check-btn");
+const res = document.getElementById("result");
+function check() {
+    if (input.value.length == 0) {
         alert("Invalid Input");
         return;
     }
-    else if(input.value.length==1){
-        res.innerHTML+=`${input.value} is a palindrome`;
-        res.style.display="block";
+    else if (input.value.length == 1) {
+        res.innerHTML += `${input.value} is a palindrome`;
+        res.style.display = "block";
         return;
     }
-    else{
-        let j=input.value.length-1;
-        for(let i=0;i<input.value.length;i++,j--){
-            if(input.value[i]===' '){
-                continue;
-            }
-            if(input.value[i]!==input.value[j]){
-                res.innerHTML+=`${input.value} is not a palindrome`;
-                res.style.display="block";
+    else {
+        let j = input.value.length - 1, i = 0;
+        const cleaned_str = input.value.replace(/\s+/g, '').toLowerCase();
+        while (i <= j) {
+            if (cleaned_str[i] !== cleaned_str[j]) {
+                res.innerHTML += `"${input.value}" is not a palindrome`;
+                res.style.display = "block";
                 return;
             }
+            i++;
+            j--;
         }
-        res.innerHTML+=`${input.value} is a palindrome`;
-                res.style.display="block";
+        res.innerHTML += `"${input.value}" is a palindrome`;
+        res.style.display = "block";
     }
-    
 }
-function clear_input(){
-    res.innerHTML=" ";
+function clear_input() {
+    res.innerHTML = " ";
 }
-button.addEventListener("click",clear_input);
-button.addEventListener("click",check);
+button.addEventListener("click", clear_input);
+button.addEventListener("click", check);
